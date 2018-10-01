ef89dd6ece552ba131d66180675d1728aa1765ae
Bipin Bhandari <bhandaribpn1996@gmail.com>
2018-10-01 21:01:30 +0545

    test

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const PORT = process.env.PORT || 5000
 var routes = require('./routes/routes.js')
 
 app.use('/routes', routes)
+app.get('/',function(req,res){
+	res.send('Welcome to Node Project');
+})
 
 
 app.listen(PORT);
