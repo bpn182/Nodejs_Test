710747f205a65a2dfc39856adb34d4771861dd1a
Bipin Bhandari <bhandaribpn1996@gmail.com>
2018-10-01 20:12:05 +0545

    adding port env info

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var app = express();
-var PORT = 8000;
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000
 
 var routes = require('./routes/routes.js')
 
